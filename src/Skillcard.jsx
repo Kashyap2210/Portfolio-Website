9ec2767137b2ad8e56f34897963b69e62a7a8021
Kashyap 2210 <kash.cdac@gmail.com>
2024-10-04 15:22:12 +0530

    Changes Made To Dark Mode Skill Section

--- src/Skillcard.jsx
+++ src/Skillcard.jsx
@@ -9,7 +9,7 @@ export default function SkillCard({ imgSource, name, isDarkMode }) {
       src={imgSource}
       alt="Logo"
       className={`w-40 h-40 m-auto object-cover rounded-2xl w-32 h-32 rounded-2xl  hover:scale-105 ease-in-out	duration-300 transition duration-300 ease-in-out transform hover:shadow-lg ${
-        isDarkMode ? "hover:shadow-yellow-600" : "hover:shadow-lightShadow "
+        isDarkMode ? "hover:shadow-yellow-600 bg-white" : "hover:shadow-lightShadow "
       }`}
     />
   );
